d00c5253bb6cbfafce569920fe1d3873fbf3413b
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-06-11 01:23:58 +0530

    Update config.py
    Signed-off-by: Decent Boy <88884010+decentboyy@users.noreply.github.com>

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "eunsemo ダ ᴍᴜsɪᴄ")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ᴀɴᴏɴ ダ ᴍᴜsɪᴄ")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "1225278379").split()))
 
